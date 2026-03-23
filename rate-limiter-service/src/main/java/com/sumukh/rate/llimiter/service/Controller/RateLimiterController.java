1e11dab3ce95b26295622fce9edd9a5224007274
sumukh shetty <sumukhshetty22@gmail.com>
2026-03-23 22:51:38 +0530

    Rate Limit for 5 reqs/min

--- rate-limiter-service/src/main/java/com/sumukh/rate/llimiter/service/Controller/RateLimiterController.java
+++ rate-limiter-service/src/main/java/com/sumukh/rate/llimiter/service/Controller/RateLimiterController.java
@@ -5,12 +5,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @RestController
 @RequestMapping("/limit")
 public class RateLimiterController {
 
+    private final Map<String, Integer> counter = new ConcurrentHashMap<>();
+    private long windowStart = System.currentTimeMillis();
+
     @GetMapping("/check")
     public boolean isAllowed(@RequestParam String clientId) {
+        long now = System.currentTimeMillis();
+
+        //Reset counter for 60 sec
+        if (now - windowStart > 60000) {
+            counter.clear();
+            windowStart = now;
+        }
+
+        counter.putIfAbsent(clientId, 0);
+        int count = counter.get(clientId);
+
+        if (count >= 5) {
+            return false;
+        }
+
+        counter.put(clientId, count+1);
         return true;
     }
 }
