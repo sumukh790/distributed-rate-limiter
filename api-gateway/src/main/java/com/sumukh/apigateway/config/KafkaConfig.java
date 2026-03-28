f183d5e813c378312bac33f20cf12511b26f1139
sumukh shetty <sumukhshetty22@gmail.com>
2026-03-28 11:34:23 +0530

    Add partitionKey for preserving order

---
+++ api-gateway/src/main/java/com/sumukh/apigateway/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.sumukh.apigateway.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public NewTopic rateLimitTopic() {
+        return TopicBuilder.name("rate-limit-events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
